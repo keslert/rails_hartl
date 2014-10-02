3b2bb2fc406f93d2dc55f7c8f963823171912d1a
Kesler Tanner <keslert@gmail.com>
2014-10-02 15:51:07 -0700

    Finished chapter 8

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   get 'users/new'
 
   root 'static_pages#home'
   get 'static_pages/help'
   get 'static_pages/about'
   get 'signup'  => 'users#new'
+  get    'login'   => 'sessions#new'
+  post   'login'   => 'sessions#create'
+  delete 'logout'  => 'sessions#destroy'
   resources :users
 
   # The priority is based upon order of creation: first created -> highest priority.
